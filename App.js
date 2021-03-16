a92f9dee58a5ea0f74df01fb3fb166ca9dfc7dda
RTurney <r.turney93@gmail.com>
2021-03-16 17:04:29 +0000

    Initialise Expo folder

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Helo world</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightsalmon',
     alignItems: 'center',
     justifyContent: 'center',
   },
