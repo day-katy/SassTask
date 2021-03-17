03ae1580d1ca176a3a584cb7c3056fbc926fb84d
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-03-17 12:09:51 +0000

    first working tests setup

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Helo world</Text>
+      <Text>Team SassTask!</Text>
       <StatusBar style="auto" />
     </View>
   );
--- app.test.js
+++ app.test.js
@@ -1,3 +1,21 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import App from './App';
+
 test('it works', () => {
   expect(true).toBeTruthy();
+});
+
+describe('<App />', () => {
+  const tree = renderer.create(<App />).toJSON();
+
+  it('has 1 child', () => {
+    expect(tree.children.length).toBe(1);
+  });
+
+  it('renders correctly', () => {
+    expect(tree).toMatchSnapshot();
+  });
+
 });
