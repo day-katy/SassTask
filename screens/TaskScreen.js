72f6ff3c299517fb753e876989d7ae3bb5494666
RTurney <r.turney93@gmail.com>
2021-03-18 11:12:56 +0000

    Add image to tasklist screen

--- screens/TaskScreen.js
+++ screens/TaskScreen.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react'; 
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, KeyboardAvoidingView, Keyboard } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, KeyboardAvoidingView, Keyboard, Image } from 'react-native';
 import Task from '../components/Task';
 
 
@@ -10,40 +10,45 @@ import Task from '../components/Task';
 const TaskScreen = () => {
   const [taskItems, setTaskItems] = useState([])
   const [task, setTask] = useState();
-  const addTask = () => { 
+  const addTask = () => {
     Keyboard.dismiss();
     setTaskItems([...taskItems, task])
     setTask('');
   }
   return (
     <View style={styles.container}>
+
       <View>
-        
         <Text style={styles.title}>Team SassTask!</Text>
         <StatusBar style="auto" />
       </View>
 
-      <View style={styles.taskList}>{
+      <View style={styles.taskList}>
+        {
           taskItems.map((item, index) => {
             return <Task key = {index} text={item} />
           })
         }
       </View>
 
+      <View style={styles.sassagotchiSection}>
+        <Image source={require('../assets/Happy-face.gif')} />
+      </View>
+
       <View style={styles.addTaskSection}>
         <View style={styles.addTask}>
         <TextInput placeholder={'Add task'} value={task} onChangeText={text => setTask(text)} />
         </View>
- 
+
         <TouchableOpacity onPress ={() => addTask()} style={styles.addTaskButton}>
           <View>
             <Text>
               +
-              </Text>
-            </View>
-          </TouchableOpacity>
-      
+            </Text>
+          </View>
+        </TouchableOpacity>
       </View>
+
     </View>
   )
 }
@@ -83,7 +88,11 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 40,
-  }
+  },
+  sassagotchiSection: {
+    width: 64,
+    height: 64,
+  },
 });
 
 export default TaskScreen;
