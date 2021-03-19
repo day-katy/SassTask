8f6e89d0e15e6de4c96cca42bc63a35644ab76db
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-03-19 14:39:13 +0000

    seperated tests into appropriate suites

--- screens/__Tests__/TaskList.test.js
+++ screens/__Tests__/TaskList.test.js
@@ -2,15 +2,6 @@ import React from 'react';
 import { render } from '@testing-library/react-native';
 import '@testing-library/jest-native/extend-expect';
 import TaskList from '../../screens/TaskList';
-import Task from '../../components/Task';
-import App from '../../App';
-
-test('renders a task', () => {
-    const { getByText, queryByText } = render(<Task text='Test task' />);
-
-    expect(getByText('Test task')).not.toBeNull();
-    expect(queryByText('ASDF')).toBeNull();
-});
 
 test('renders the app title', () => {
     const { getByTestId } = render(<TaskList TestId='title'/>);
@@ -22,10 +13,4 @@ test('renders the right background color', () => {
     const { getByTestId } = render(<TaskList TestId='container'/>);
 
     expect(getByTestId('container')).toHaveStyle({ backgroundColor: 'lightsalmon' });
-});
-
-test('renders the right background color across entire app', () => {
-    const { getByTestId } = render(<App TestId='entireAppWrapper'/>);
-
-    expect(getByTestId('entireAppWrapper')).toHaveStyle({ backgroundColor: 'lightsalmon' });
 });
