9dbadd4e05aa9e7be4a2f1bdd03edd03ec61d7a3
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-03-24 17:54:56 +0000

    family index changes after reset

--- components/Monster.js
+++ components/Monster.js
@@ -6,60 +6,69 @@ export const monster = ({ imageSrc, setImageSrc, completedTasks, rating, setRati
 
   const [monsterStyle, setMonsterStyle] = useState(styles.monsterSmall)
 
-  let family = [require('../assets/Sassagotchi-ghost.gif'), require('../assets/Sassagotchi-egg-dying.gif'), require('../assets/Sassagotchi-egg-coloured.gif'), require('../assets/Sassagotchi-final-egg.gif'), require('../assets/SasSquash.gif'), require('../assets/SnapSquash.gif'), require('../assets/Ascended-Snapsquash.gif')]
-  
+  const [famIndex, setFamIndex] = useState(0)
+
+  let firstFam = [require('../assets/Sassagotchi-ghost.gif'), require('../assets/Sassagotchi-egg-dying.gif'), require('../assets/Sassagotchi-egg-coloured.gif'), require('../assets/Sassagotchi-final-egg.gif'), require('../assets/SasSquash.gif'), require('../assets/SnapSquash.gif'), require('../assets/Ascended-Snapsquash.gif')]
+  let secondFam = [require('../assets/number0.png'), require('../assets/number1.png'), require('../assets/number2.png'), require('../assets/number3.png'), require('../assets/number4.png'), require('../assets/number5.png'), require('../assets/number6.png')]
+  let famIndex1 = 0
+  let famIndex2 = 2
+  let families = [firstFam, secondFam]
+
+
+
 
   const monsterPicker = () => {
     if (rating > 10){
       setMonsterStyle(styles.monsterLarge)
-      setImageSrc(family[6])
+      setImageSrc(families[famIndex][famIndex2 += 4])
       setMessage('Your efforts have allowed me to ascend. I can go no further. Click reset to try again with a new Sasscot...')
     } else if(rating >7) {
           setMonsterStyle(styles.monsterLarge)
-          setImageSrc(family[5])
+          setImageSrc(families[famIndex][famIndex2 += 3])
       } else if(rating >6) {
           setMonsterStyle(styles.monsterMedium)
-          setImageSrc(family[5])
+          setImageSrc(families[famIndex][famIndex2 += 3])
       } else if(rating >5) {
         setMonsterStyle(styles.monsterSmall)
-        setImageSrc(family[5])
+        setImageSrc(families[famIndex][famIndex2 += 3])
       } else if(rating >4) {
         setMonsterStyle(styles.monsterLarge)
-        setImageSrc(family[4])
+        setImageSrc(families[famIndex][famIndex2 += 2])
       } else if(rating >3) {
         setMonsterStyle(styles.monsterMedium)
-        setImageSrc(family[4])
+        setImageSrc(families[famIndex][famIndex2 += 2])
       } else if(rating >2) {
         setMonsterStyle(styles.monsterSmall)
-        setImageSrc(family[4])
+        setImageSrc(families[famIndex][famIndex2 += 2])
       } else if (rating > 1) {
         setMonsterStyle(styles.monsterLarge)
-        setImageSrc(family[3])
+        setImageSrc(families[famIndex][famIndex2 += 1])
       } else if (rating > 0) {
         setMonsterStyle(styles.monsterMedium)
-        setImageSrc(family[2])
+        setImageSrc(families[famIndex][famIndex2])
       } else if (rating == 0) {
         setMonsterStyle(styles.monsterSmall)
-        setImageSrc(family[2])
+        setImageSrc(families[famIndex][famIndex2])
       } else if (rating < 0 && rating > -2) {
         setMonsterStyle(styles.monsterSmall)
-        setImageSrc(family[1])
+        setImageSrc(families[famIndex][famIndex2 -= 1])
       }
        else if (rating < -2) {
         setMonsterStyle(styles.monsterSmall)
         setMessage("You killed me! Click reset to try again")
-        setImageSrc(family[0])
+        setImageSrc(families[famIndex][famIndex2 -= 2])
       }
 
   }
 
   const resetButton = () => {
-    console.log(family[0])
+    console.log(firstFam[0])
     if(rating > 10) {
       setRating(0)
       setMessage("Congratulations. Here is a new monster for you to try with...")
       setMonsterStyle(styles.monsterSmall)
       setImageSrc(require('../assets/Sassagotchi-egg-coloured.gif'))
+      setFamIndex(1)
     } else if(rating > -2) {
       setMessage('You must live with your choices')
     } else {
@@ -67,6 +76,7 @@ export const monster = ({ imageSrc, setImageSrc, completedTasks, rating, setRati
       setMessage("You have been given another chance... don't mess it up")
       setMonsterStyle(styles.monsterSmall)
       setImageSrc(require('../assets/Sassagotchi-egg-coloured.gif'))
+      setFamIndex(1)
     }
   }
 
--- components/test.js
+++
@@ -1,8 +0,0 @@
-import { enableExpoCliLogging } from "expo/build/logs/Logs"
-
-
-allfamilys = [[firstfam],[secondfam],[thirdfam]]
-
-firstfam= [death,bad-egg,egg,small,large,ascended]
-secondfam= [death,bad-egg,egg,small,large,ascended]
-thirdfam= [death,bad-egg,egg,small,large,ascended]
