956611ea328c0348c7655d7b656368640437f7a0
Katy Day <day.katy@gmail.com>
2021-03-25 11:57:44 +0000

    cleaning up tasklist.js

--- components/TaskList.js
+++ components/TaskList.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
 import Task from '../components/Task';
 
-import { sayHi, updateTaskStatus } from '../src/firebase/utils'
+import { updateTaskStatus } from '../src/firebase/utils'
 
 const TaskList = (props) => {
 
