03c3fae9e022c6345df532addf07121f75bdabcf
RTurney <r.turney93@gmail.com>
2021-03-26 15:41:26 +0000

    Add sassagotchi to tasks

--- components/Task.js
+++ components/Task.js
@@ -6,7 +6,9 @@ const Task = (props) => {
   return (
     <View style={styles.item}>
       <View style={styles.itemLeft}>
-        <View style={styles.icon}><Image source={require('../assets/logo.gif')}/></View>
+        <View >
+          <Image style={styles.icon} source={require('../assets/logo.gif')}/>
+        </View>
         <Text style={styles.itemText}>{props.text}</Text>
       </View>
     </View>
